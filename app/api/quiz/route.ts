89d5565f9752c52c32e1ee64ebf03a72e36b50ad
Chan <chan@iPod-Classic-128GB.local>
2024-05-28 23:44:01 +0100

    adjusted

--- app/api/quiz/route.ts
+++ app/api/quiz/route.ts
@@ -6,15 +6,14 @@ export async function GET(req: NextRequest, res: NextResponse) {
   await dbConnect();
   const { userId } = getAuth(req);
 
-  if (userId) {
-    try {
-      const quizzes = await Quiz.find({});
-      console.log(quizzes, "quizze");
-      return NextResponse.json({ data: quizzes }, { status: 200 });
-    } catch (err: any) {
-      return NextResponse.json({ error: err.message }, { status: 500 });
-    }
-  } else {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
+  // if (userId) {
+  //   try {
+  //     const quizzes = await Quiz.find({});
+  //     return NextResponse.json({ data: quizzes }, { status: 200 });
+  //   } catch (err: any) {
+  //     return NextResponse.json({ error: err.message }, { status: 500 });
+  //   }
+  // } else {
+  //   return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  // }
 }
