a5aa0913e5f270f208494999e9798ef2a1a9f57e
Chan <chan@ipodclasic128gb.home>
2024-07-03 11:49:28 +0100

    added secret

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -6,6 +6,7 @@ import { TroubleShoot } from "@/lib/models/TroubleShoot";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 const s3Client = new S3Client({
+  //secret
   region: process.env.NEXT_PUBLIC_AWS_S3_REGION,
   credentials: {
     accessKeyId: process.env.NEXT_PUBLIC_AWS_S3_ACCESS_KEY_ID,
