95daf4ca3bfeacc3ffcaad0504aa96c4f06f89e5
Chan <chan@ipodclasic128gb.home>
2024-07-03 11:38:54 +0100

    set up S3 bucket

---
+++ app/api/image/route.ts
@@ -0,0 +1,53 @@
+import { NextResponse, NextRequest } from "next/server";
+import dbConnect from "@/lib/dbConnect";
+import Quiz from "@/lib/models/Quiz";
+import { getAuth } from "@clerk/nextjs/server";
+import { TroubleShoot } from "@/lib/models/TroubleShoot";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+
+const s3Client = new S3Client({
+  region: process.env.NEXT_PUBLIC_AWS_S3_REGION,
+  credentials: {
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_S3_SECRET_ACCESS_KEY,
+  },
+});
+
+async function uploadFileToS3(fileBuffer, fileName, fileType) {
+  const uploadParams = {
+    Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME,
+    Key: fileName,
+    Body: fileBuffer,
+    ContentType: fileType,
+  };
+
+  const command = new PutObjectCommand(uploadParams);
+  await s3Client.send(command);
+
+  return fileName;
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const formData = await req.formData();
+    const file = formData.get("file");
+
+    if (!file || !(file instanceof File)) {
+      return NextResponse.json(
+        { error: "File is required and must be of type File." },
+        { status: 400 }
+      );
+    }
+
+    const buffer = Buffer.from(await file.arrayBuffer());
+    const fileName = await uploadFileToS3(buffer, file.name, file.type);
+
+    return NextResponse.json({ success: true, fileName });
+  } catch (err: any) {
+    console.error("Error fetching TroubleShoots:", err);
+    return NextResponse.json(
+      { error: err.message || "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
