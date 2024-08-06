b8ec721382668d3de5dcee86ce2ec1ec5e0d6b92
Chan <chan@iPod-Classic-128GB.local>
2024-08-06 15:00:19 +0100

    added postmark

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,16 +1,39 @@
+"use client";
+import { useState } from "react";
 import NewsLatterBox from "./NewsLatterBox";
 
 const Contact = () => {
+  const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const sendEmail = async () => {
+    try {
+      const res = await fetch("/api/sendEmail", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          message: message,
+          name: name,
+          email: email,
+        }),
+      });
+    } catch (e) {
+      alert(e);
+    }
+  };
   return (
     <section
       id="contact"
-      className="bg-primaryColor overflow-hidden py-16 md:py-20 lg:py-28"
+      className="overflow-hidden bg-primaryColor py-16 md:py-20 lg:py-28"
     >
       <div className="container">
         {/* <div className="-mx-4 flex flex-wrap"> */}
         <div className="w-full px-4  xl:w-10/12">
           <div
-            className="wow fadeInUp mb-12 rounded-md bg-thirdColor py-11 px-8 dark:bg-thirdColor sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
+            className="wow fadeInUp mb-12 rounded-md bg-thirdColor px-8 py-11 dark:bg-thirdColor sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
             data-wow-delay=".15s
               "
           >
@@ -33,7 +56,10 @@ const Contact = () => {
                     <input
                       type="text"
                       placeholder="Enter your name"
-                      className="w-full rounded-md border border-transparent py-3 px-6 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-primaryColor dark:shadow-signUp"
+                      className="w-full rounded-md border border-transparent px-6 py-3 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-primaryColor dark:shadow-signUp"
+                      onChange={(e) => {
+                        setName(e.target.value);
+                      }}
                     />
                   </div>
                 </div>
@@ -46,9 +72,12 @@ const Contact = () => {
                       Your Email
                     </label>
                     <input
+                      onChange={(e) => {
+                        setEmail(e.target.value);
+                      }}
                       type="email"
                       placeholder="Enter your email"
-                      className="w-full rounded-md border border-transparent py-3 px-6 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-primaryColor dark:shadow-signUp"
+                      className="w-full rounded-md border border-transparent px-6 py-3 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-primaryColor dark:shadow-signUp"
                     />
                   </div>
                 </div>
@@ -61,15 +90,23 @@ const Contact = () => {
                       Your Message
                     </label>
                     <textarea
+                      onChange={(e) => {
+                        setMessage(e.target.value);
+                      }}
                       name="message"
                       rows={5}
                       placeholder="Enter your Message"
-                      className="w-full resize-none rounded-md border border-transparent py-3 px-6 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-primaryColor dark:shadow-signUp"
+                      className="w-full resize-none rounded-md border border-transparent px-6 py-3 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-primaryColor dark:shadow-signUp"
                     ></textarea>
                   </div>
                 </div>
                 <div className="w-full px-4">
-                  <button className="rounded-md bg-fifthColor py-4 px-9 text-base font-medium text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
+                  <button
+                    className="rounded-md bg-fifthColor px-9 py-4 text-base font-medium text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
+                    onClick={() => {
+                      sendEmail();
+                    }}
+                  >
                     Submit Ticket
                   </button>
                 </div>
