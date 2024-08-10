54849783b6faaa227c095176586c37d0017f30cb
Chan <chan@iPod-Classic-128GB.local>
2024-08-10 09:24:28 +0100

    added tos

---
+++ components/Tos/index.tsx
@@ -0,0 +1,209 @@
+/* eslint-disable react/no-unescaped-entities */
+"use client";
+
+import SectionTitle from "../Common/SectionTitle";
+
+const Tos = () => {
+  return (
+    <section
+      id="tos"
+      className="relative z-10 bg-thirdColor py-16 md:py-20 lg:py-28"
+    >
+      <div className="container">
+        <SectionTitle
+          title="Terms of Service"
+          paragraph=""
+          center
+          width="665px"
+        />
+
+        <div className="bg-primaryColor justify-center items-center p-5 rounded-xl">
+          <div className="text-white">
+            <h1>Last Updated: 10th August 2024</h1> <br />
+            <p>
+              These Terms of Service ("Terms") govern your access to and use of
+              Trouble-Shush ("Service"), operated by Hanio ("Company," "we,"
+              "our," or "us"). By accessing or using our Service, you agree to
+              be bound by these Terms. If you do not agree to these Terms, do
+              not use the Service.
+            </p>{" "}
+            <br />
+            <h3>1. Use of Service</h3> <br />
+            <h4>1.1 Eligibility</h4>
+            <p>
+              You must be at least 18 years old and legally capable of entering
+              into binding contracts under UK law to use the Service.
+            </p>{" "}
+            <br />
+            <h4>1.2 Account Registration</h4>
+            <p>
+              To access certain features of the Service, you may be required to
+              create an account. You are responsible for maintaining the
+              confidentiality of your account information and agree to accept
+              responsibility for all activities that occur under your account.
+            </p>{" "}
+            <br />
+            <h4>1.3 Acceptable Use</h4>
+            <p>
+              You agree not to use the Service in any way that is unlawful or
+              prohibited by these Terms. This includes, but is not limited to:
+            </p>
+            <ul>
+              <li>
+                Uploading or sharing content that is illegal, harmful, or
+                violates the rights of others.
+              </li>
+              <li>
+                Using the Service to build or support any application that
+                competes with the Service.
+              </li>
+              <li>Interfering with the security of the Service.</li>
+            </ul>{" "}
+            <br />
+            <h3>2. User-Generated Content</h3> <br />
+            <h4>2.1 Ownership and License</h4>
+            <p>
+              By uploading content, including images and troubleshooting steps
+              ("User Content"), you grant the Company a non-exclusive,
+              royalty-free, worldwide, sublicensable, and transferable license
+              to use, reproduce, modify, distribute, display, and perform your
+              User Content in connection with the Service.
+            </p>{" "}
+            <br />
+            <h4>2.2 Responsibility for Content</h4>
+            <p>
+              You are solely responsible for the User Content you upload and
+              represent that you own all rights to such content or have obtained
+              the necessary permissions.
+            </p>{" "}
+            <br />
+            <h4>2.3 Prohibited Content</h4> <br />
+            <p>You agree not to upload any content that:</p>
+            <ul>
+              <li>Infringes on intellectual property rights.</li>
+              <li>Contains harmful or malicious software.</li>
+              <li>Is pornographic, defamatory, or otherwise inappropriate.</li>
+            </ul>{" "}
+            <br />
+            <h3>3. Fees, Payment, and Refunds</h3> <br />
+            <h4>3.1 Fees</h4>
+            <p>
+              [Outline any fees associated with the use of the Service,
+              including subscription models and payment terms.]
+            </p>{" "}
+            <br />
+            <h4>3.2 Non-Refundable Policy</h4>
+            <p>
+              All fees paid for the Service are non-refundable, except as
+              required by law. By purchasing or subscribing to the Service, you
+              acknowledge and agree that all sales are final and that you will
+              not be entitled to a refund, credit, or exchange for any reason.
+            </p>{" "}
+            <br />
+            <h4>3.3 Statutory Rights</h4>
+            <p>
+              Notwithstanding the above, nothing in these Terms is intended to
+              limit your statutory rights under UK law. If the Service is
+              faulty, not as described, or not provided with reasonable care and
+              skill, you may be entitled to a remedy, including a refund, as
+              required by the Consumer Rights Act 2015 and other applicable
+              legislation.
+            </p>{" "}
+            <br />
+            <h4>3.4 Cooling-Off Period</h4>
+            <p>
+              If you are a consumer and have purchased the Service online, you
+              may have the right to cancel your purchase within 14 days of the
+              transaction under the Consumer Contracts (Information,
+              Cancellation, and Additional Charges) Regulations 2013. However,
+              by accessing and using the Service immediately after purchase, you
+              agree to waive your right to this cooling-off period.
+            </p>{" "}
+            <br />
+            <h4>3.5 Taxes</h4> <br />
+            <p>
+              You are responsible for all applicable taxes related to your use
+              of the Service.
+            </p>{" "}
+            <br />
+            <h3>4. Intellectual Property</h3> <br />
+            <h4>4.1 Ownership</h4>
+            <p>
+              The Service and all materials therein, including software, design,
+              and content (excluding User Content), are owned by the Company and
+              are protected by copyright, trademark, and other intellectual
+              property laws.
+            </p>{" "}
+            <br />
+            <h4>4.2 Restrictions</h4>
+            <p>
+              You may not copy, modify, distribute, or reverse engineer any part
+              of the Service without the Company's express written consent.
+            </p>{" "}
+            <br />
+            <h3>5. Termination</h3> <br />
+            <h4>5.1 Termination by You</h4>
+            <p>
+              You may terminate your account at any time by contacting customer
+              support.
+            </p>{" "}
+            <br />
+            <h4>5.2 Termination by Us</h4>
+            <p>
+              We reserve the right to suspend or terminate your account if you
+              violate these Terms or engage in conduct that we deem harmful to
+              the Service or other users.
+            </p>{" "}
+            <br />
+            <h3>6. Disclaimers and Limitation of Liability</h3>
+            <h4>6.1 Disclaimers</h4>
+            <p>
+              The Service is provided "as is" and "as available." We make no
+              warranties or representations regarding the reliability, accuracy,
+              or availability of the Service.
+            </p>{" "}
+            <br />
+            <h4>6.2 Limitation of Liability</h4>
+            <p>
+              To the maximum extent permitted by law, the Company shall not be
+              liable for any indirect, incidental, special, or consequential
+              damages arising out of or in connection with the use of the
+              Service.
+            </p>{" "}
+            <br />
+            <h3>7. Indemnification</h3>
+            <p>
+              You agree to indemnify and hold harmless the Company from any
+              claims, losses, damages, liabilities, including legal fees,
+              arising out of your use of the Service or violation of these
+              Terms.
+            </p>{" "}
+            <br />
+            <h3>8. Governing Law</h3>
+            <p>
+              These Terms are governed by and construed in accordance with the
+              laws of England and Wales. Any disputes arising out of these Terms
+              or the use of the Service shall be subject to the exclusive
+              jurisdiction of the courts of England and Wales.
+            </p>{" "}
+            <br />
+            <h3>9. Changes to the Terms</h3>
+            <p>
+              We may update these Terms from time to time. If we make changes,
+              we will notify you by revising the date at the top of the Terms or
+              by providing additional notice.
+            </p>{" "}
+            <br />
+            <h3>10. Contact Information</h3>
+            <p>
+              If you have any questions about these Terms, please send us an
+              email via the support portal.
+            </p>{" "}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Tos;
