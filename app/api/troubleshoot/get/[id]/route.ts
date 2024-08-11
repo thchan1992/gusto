3c6f7a113ec47b5fdbcf9a8176c0fa2c7ed70cb7
Chan <chan@iPod-Classic-128GB.local>
2024-08-11 13:36:45 +0100

    adjusteD

--- app/api/troubleshoot/get/[id]/route.ts
+++ app/api/troubleshoot/get/[id]/route.ts
@@ -12,7 +12,7 @@ const ratelimit = new Ratelimit({
   limiter: Ratelimit.slidingWindow(5, "10s"),
 });
 
-export const config = { runtime: "edge" };
+export const runtime = "edge";
 export async function GET(
   req: NextRequest,
   // res: NextResponse,
