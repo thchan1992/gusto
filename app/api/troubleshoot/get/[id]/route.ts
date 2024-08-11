6a3a91ebdb64b3e546f72f8cc44bb8acaebc0562
Chan <chan@iPod-Classic-128GB.local>
2024-08-11 13:32:49 +0100

    added rate limit

--- app/api/troubleshoot/get/[id]/route.ts
+++ app/api/troubleshoot/get/[id]/route.ts
@@ -1,14 +1,36 @@
 import { NextResponse, NextRequest } from "next/server";
+import { Ratelimit } from "@upstash/ratelimit";
 import dbConnect from "@/lib/dbConnect";
+import { kv } from "@vercel/kv";
 import Quiz from "@/lib/models/Quiz";
 import { getAuth } from "@clerk/nextjs/server";
 import { TroubleShoot } from "@/lib/models/TroubleShoot";
 import { auth } from "@clerk/nextjs/server";
+
+const ratelimit = new Ratelimit({
+  redis: kv,
+  limiter: Ratelimit.slidingWindow(5, "10s"),
+});
+
+export const config = { runtime: "edge" };
 export async function GET(
-  req: Request,
+  req: NextRequest,
   // res: NextResponse,
   { params }: { params: { id: string } }
 ) {
+  const ip = req.ip ?? "127.0.0.1";
+  const { limit, reset, remaining } = await ratelimit.limit(ip);
+  if (remaining === 0) {
+    return new NextResponse(JSON.stringify({ error: "Rate limit exceeded" }), {
+      status: 429,
+      headers: {
+        "X-RateLimit-Limit": limit.toString(),
+        "X-RateLimit-Remaining": remaining.toString(),
+        "X-RateLimit-Reset": reset.toString(),
+      },
+    });
+  }
+
   const troubleShootId = params.id;
   const { userId } = auth();
   if (!userId) {
